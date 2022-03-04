8c1ac4ad71588c97797aa63fc043eddcfca8c613
sa1583 <sa1583@naver.com>
2022-03-04 14:49:37 +0900

    first commit

---
+++ app/src/main/java/fastcampus/aop/part2/electronicframe/MainActivity.kt
@@ -0,0 +1,65 @@
+package fastcampus.aop.part2.electronicframe
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val addPhotoButton: Button by lazy {
+        findViewById(R.id.addPhotoButton)
+    }
+
+    private val startPhotoFrameModeButton: Button by lazy {
+        findViewById(R.id.startPhotoFrameModeButton)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initAddPhotoButton()
+        initStartPhotoFrameModeButton()
+    }
+
+    private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // TODO 권한이 잘 부여되었으므로 갤러리에서 사진을 선택하는 기능
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    // TODO 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요하빈다")
+            .setMessage("사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { dialog, which ->
+
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun initStartPhotoFrameModeButton() {
+
+    }
+
+
+}
